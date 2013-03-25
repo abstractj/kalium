9b54e65831313533c421b1d2a46e52bfc2be70ec
Bruno Oliveira <bruno@abstractj.org>
2013-03-25 16:59:53 -0300

    Inclusion of the method toHex

--- src/main/java/org/abstractj/kalium/crypto/Hash.java
+++ src/main/java/org/abstractj/kalium/crypto/Hash.java
@@ -2,21 +2,30 @@ package org.abstractj.kalium.crypto;
 
 import org.abstractj.kalium.Sodium;
 import org.abstractj.kalium.Sodium.CSodium;
+import org.abstractj.kalium.util.Hex;
 
 public class Hash {
 
     private static final CSodium sodium = Sodium.getInstance();
 
-    public static byte[] sha256(String message) {
-        byte[] buffer = new byte[32];
+    public static final int SHA256BYTES = 32;
+    public static final int SHA512BYTES = 64;
+
+    private static byte[] buffer;
+
+    public Hash sha256(String message) {
+        buffer = new byte[SHA256BYTES];
         sodium.crypto_hash_sha256_ref(buffer, message, message.length());
-        return buffer;
+        return this;
     }
 
-    public static byte[] sha512(String message) {
-        byte[] buffer = new byte[64];
+    public Hash sha512(String message) {
+        buffer = new byte[SHA512BYTES];
         sodium.crypto_hash_sha512_ref(buffer, message, message.length());
-        return buffer;
+        return this;
     }
 
+    public String toHex() {
+        return Hex.encodeHexString(buffer);
+    }
 }
--- src/test/java/org/abstractj/kalium/crypto/HashTest.java
+++ src/test/java/org/abstractj/kalium/crypto/HashTest.java
@@ -1,24 +1,25 @@
 package org.abstractj.kalium.crypto;
 
-import org.abstractj.kalium.util.Hex;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class HashTest {
 
+    private Hash hash = new Hash();
+
     @Test
     public void testSha256() throws Exception {
         String expected = "ee9d62778c8b664aa8501af83ec4738e01d20f2cdca133208c7bf66cbcaa37b8";
-        byte[] hash = Hash.sha256("sodium");
-        assertEquals("Hash value must be the same", expected, Hex.encodeHexString(hash));
+        String result = hash.sha256("sodium").toHex();
+        assertEquals("Hash value must be the same", expected, result);
     }
 
     @Test
     public void testSha512() throws Exception {
-        String value = "961b4af816284b41547aaff17bef5eae144f4a29dafa7b1819100e0e8deb93bea" +
+        String expected = "961b4af816284b41547aaff17bef5eae144f4a29dafa7b1819100e0e8deb93bea" +
                 "53946b38c579ba6a74162f3bdbc5adc672d4ec52425971d3072c37e18430fc0";
-        byte[] hash = Hash.sha512("sodium");
-        assertEquals("Hash value must be the same", value, Hex.encodeHexString(hash));
+        String result = hash.sha512("sodium").toHex();
+        assertEquals("Hash value must be the same", expected, result);
     }
 }
