a5149a07239f81c9d57d182ed1719781985a2d77
Bruno Oliveira <bruno@abstractj.org>
2013-03-25 16:05:41 -0300

    Initial implementation with SHA-256 and SHA-512

--- src/main/java/org/abstractj/kalium/Sodium.java
+++ src/main/java/org/abstractj/kalium/Sodium.java
@@ -1,35 +1,18 @@
 package org.abstractj.kalium;
 
-import jnr.ffi.annotations.In;
-import jnr.ffi.annotations.LongLong;
-import jnr.ffi.annotations.Out;
 import org.abstractj.kalium.util.Loader;
-
-import java.nio.ByteBuffer;
 
 public class Sodium {
 
     public static final String LIBNAME = "sodium";
 
     public interface CSodium {
-        public int randombytes_random();
         public String sodium_version_string();
-        public int crypto_hash_sha256(@Out ByteBuffer p1, @In ByteBuffer p2, @LongLong long l);
-        public int crypto_hash_sha512(@Out ByteBuffer p1, @In ByteBuffer p2, @LongLong long l);
+        public int crypto_hash_sha256_ref(byte[] buffer, String message, long sizeof);
+        public int crypto_hash_sha512_ref(byte[] buffer, String message, long sizeof);
     }
 
-    public static void main(String argv[]) {
-        CSodium sodium = Loader.lib(LIBNAME, CSodium.class);
-        ByteBuffer foo = ByteBuffer.allocateDirect(1000000);
-        ByteBuffer bar = ByteBuffer.allocateDirect(1000000);
-
-        try {
-            System.out.println("Sodium release: " + sodium.sodium_version_string());
-            System.out.println(sodium.crypto_hash_sha512(foo, bar, 1000000L));
-        } catch (Exception e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-        }
-
+    public static CSodium getInstance() {
+        return Loader.lib(LIBNAME, CSodium.class);
     }
-
 }
--- src/main/java/org/abstractj/kalium/crypto/Hash.java
+++ src/main/java/org/abstractj/kalium/crypto/Hash.java
@@ -1,4 +1,22 @@
 package org.abstractj.kalium.crypto;
 
+import org.abstractj.kalium.Sodium;
+import org.abstractj.kalium.Sodium.CSodium;
+
 public class Hash {
+
+    private static final CSodium sodium = Sodium.getInstance();
+
+    public static byte[] sha256(String message) {
+        byte[] buffer = new byte[32];
+        sodium.crypto_hash_sha256_ref(buffer, message, message.length());
+        return buffer;
+    }
+
+    public static byte[] sha512(String message) {
+        byte[] buffer = new byte[64];
+        sodium.crypto_hash_sha512_ref(buffer, message, message.length());
+        return buffer;
+    }
+
 }
