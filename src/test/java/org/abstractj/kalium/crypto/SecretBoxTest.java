47113dd526c27f609e8b5499711c3f1312a1be25
Bruno Oliveira <bruno@abstractj.org>
2013-03-30 03:54:51 -0300

    Licensen

--- src/test/java/org/abstractj/kalium/crypto/SecretBoxTest.java
+++ src/test/java/org/abstractj/kalium/crypto/SecretBoxTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2013 Bruno Oliveira, and individual contributors
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package org.abstractj.kalium.crypto;
 
 import org.abstractj.kalium.encoders.Hex;
@@ -56,10 +72,9 @@ public class SecretBoxTest {
 
         byte[] nonce = Hex.decodeHexString(BOX_NONCE);
         byte[] expectedMessage = Hex.decodeHexString(BOX_MESSAGE);
-        byte[] ciphertext = Hex.decodeHexString(BOX_CIPHERTEXT);
+        byte[] ciphertext = box.encrypt(nonce, expectedMessage);
 
-        byte[] result = box.encrypt(nonce, expectedMessage);
-        byte[] message = box.decrypt(nonce, result);
+        byte[] message = box.decrypt(nonce, ciphertext);
 
         assertTrue("failed to decrypt ciphertext", Arrays.equals(message, expectedMessage));
     }
