8ace2a7ada8c4581817f3268de6c92f870414304
vshaipov <vit1307@yandex.ru>
2015-08-09 20:25:41 +0200

    Used static imports

--- src/main/java/org/abstractj/kalium/crypto/Stream.java
+++ src/main/java/org/abstractj/kalium/crypto/Stream.java
@@ -1,14 +1,14 @@
 package org.abstractj.kalium.crypto;
 
 import static org.abstractj.kalium.NaCl.sodium;
+import static org.abstractj.kalium.NaCl.Sodium.AES_128_CTR_KEYBYTES;
 import static org.abstractj.kalium.NaCl.Sodium.BOXZERO_BYTES;
 import static org.abstractj.kalium.NaCl.Sodium.CRYPTO_STREAM_AES_128_CTR_NONCEBYTES;
+import static org.abstractj.kalium.NaCl.Sodium.ZERO_BYTES;
 import static org.abstractj.kalium.crypto.Util.checkLength;
 import static org.abstractj.kalium.crypto.Util.isValid;
 import static org.abstractj.kalium.crypto.Util.prependZeros;
 import static org.abstractj.kalium.crypto.Util.removeZeros;
-
-import org.abstractj.kalium.NaCl;
 
 public class Stream {
 
@@ -16,12 +16,12 @@ public class Stream {
 
 	public Stream(byte[] key) {
 		this.key = key;
-		checkLength(key, NaCl.Sodium.AES_128_CTR_KEYBYTES);
+		checkLength(key, AES_128_CTR_KEYBYTES);
 	}
 
 	public byte[] encrypt(byte[] nonce, byte[] message) {
 		checkLength(nonce, CRYPTO_STREAM_AES_128_CTR_NONCEBYTES);
-		byte[] msg = prependZeros(NaCl.Sodium.ZERO_BYTES, message);
+		byte[] msg = prependZeros(ZERO_BYTES, message);
 		byte[] ct = new byte[msg.length];
 
 		isValid(sodium().crypto_stream_aes128ctr_xor(ct, msg, msg.length,
@@ -38,7 +38,7 @@ public class Stream {
 		isValid(sodium().crypto_stream_aes128ctr_xor(message, ct,
 				message.length, nonce, key), "Decryption failed");
 
-		return removeZeros(NaCl.Sodium.ZERO_BYTES, message);
+		return removeZeros(ZERO_BYTES, message);
 	}
 
 }
