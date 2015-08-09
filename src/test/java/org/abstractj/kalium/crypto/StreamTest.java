2b14184ec73109a2052dde1bac51c4426228e8f6
Vitaly Shaipov <vitaly.shaipov@ctp.com>
2015-08-09 22:14:35 +0200

    Refactoring in order to process a pull request to main kalium project

--- src/test/java/org/abstractj/kalium/crypto/StreamTest.java
+++ src/test/java/org/abstractj/kalium/crypto/StreamTest.java
@@ -1,33 +1,34 @@
 package org.abstractj.kalium.crypto;
 
-import org.abstractj.kalium.NaCl;
+import static org.abstractj.kalium.encoders.Encoder.HEX;
+import static org.abstractj.kalium.fixture.TestVectors.AES128_KEY;
+import static org.abstractj.kalium.fixture.TestVectors.AES128_MESSAGE;
+import static org.abstractj.kalium.NaCl.Sodium.CRYPTO_STREAM_AES_128_CTR_NONCEBYTES;
+
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class StreamTest {
 	private static Stream stream;
-	private static byte[] key;
 	private static byte[] nonce;
 
-	private static byte[] message = "Hello World!".getBytes();
+	private static byte[] message = AES128_MESSAGE.getBytes();
 
 	@BeforeClass
 	public static void init() {
 		Random random = new Random();
-		key = random.randomBytes(NaCl.Sodium.AES_128_CTR_KEYBYTES);
-
-		stream = new Stream(key);
+		stream = new Stream(HEX.decode(AES128_KEY));
 
 		nonce = random
-				.randomBytes(NaCl.Sodium.CRYPTO_STREAM_AES_128_CTR_NONCEBYTES);
+				.randomBytes(CRYPTO_STREAM_AES_128_CTR_NONCEBYTES);
 	}
 
 	@Test
 	public void testEncryption() {
 		byte[] encryptedMessage = stream.encrypt(nonce, message);
-
 		byte[] decryptedMessage = stream.decrypt(nonce, encryptedMessage);
-
-		System.out.println(new String(decryptedMessage));
+		
+		Assert.assertEquals(AES128_MESSAGE, new String(decryptedMessage));
 	}
 }
--- src/test/java/org/abstractj/kalium/fixture/TestVectors.java
+++ src/test/java/org/abstractj/kalium/fixture/TestVectors.java
@@ -84,4 +84,10 @@ public class TestVectors {
     public static final String SIGN_SIGNATURE = "6bd710a368c1249923fc7a1610747403040f0cc30815a00f9ff548a896bbda0b" +
             "4eb2ca19ebcf917f0f34200a9edbad3901b64ab09cc5ef7b9bcc3c40c0ff7509";
     public static final String SIGN_PUBLIC = "77f48b59caeda77751ed138b0ec667ff50f8768c25d48309a8f386a2bad187fb";
+    
+    /**
+     * AES 128 test vectors
+     */
+    public static final String AES128_MESSAGE = "Hello World!";
+    public static final String AES128_KEY = "26eee63e1216b927e49871e52f7129a8";
 }
