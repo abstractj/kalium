d54b368de20ec51b2576ab8997e5216a80bd5ae5
vshaipov <vit1307@yandex.ru>
2015-08-09 20:13:41 +0200

    solved code formatting issue

--- src/main/java/org/abstractj/kalium/NaCl.java
+++ src/main/java/org/abstractj/kalium/NaCl.java
@@ -24,130 +24,112 @@ import jnr.ffi.types.u_int64_t;
 
 public class NaCl {
 
-	public static Sodium sodium() {
-		Sodium sodium = SingletonHolder.SODIUM_INSTANCE;
+    public static Sodium sodium() {
+        Sodium sodium = SingletonHolder.SODIUM_INSTANCE;
 
-		if (!(sodium.sodium_version_string().compareTo("0.4.0") >= 0)) {
-			String message = String.format(
-					"Unsupported libsodium version: %s. Please update",
-					sodium.sodium_version_string());
-			throw new UnsupportedOperationException(message);
-		}
-		return sodium;
-	}
+        if(!(sodium.sodium_version_string().compareTo("0.4.0") >= 0)){
+            String message = String.format("Unsupported libsodium version: %s. Please update",
+                    sodium.sodium_version_string());
+            throw new UnsupportedOperationException(message);
+        }
+        return sodium;
+    }
 
-	private static final String LIBRARY_NAME = "sodium";
+    private static final String LIBRARY_NAME = "sodium";
 
-	private static final class SingletonHolder {
-		public static final Sodium SODIUM_INSTANCE = LibraryLoader
-				.create(Sodium.class).search("/usr/local/lib")
-				.search("/opt/local/lib").search("lib").load(LIBRARY_NAME);
+    private static final class SingletonHolder {
+        public static final Sodium SODIUM_INSTANCE = LibraryLoader.create(Sodium.class)
+                .search("/usr/local/lib")
+                .search("/opt/local/lib")
+                .search("lib")
+                .load(LIBRARY_NAME);
 
-	}
+    }
 
-	private NaCl() {
-	}
+    private NaCl() {
+    }
 
-	public interface Sodium {
+    public interface Sodium {
 
-		/**
-		 * This function isn't thread safe. Be sure to call it once, and before
-		 * performing other operations.
-		 * 
-		 * Check libsodium's documentation for more info.
-		 */
-		public int sodium_init();
+        /**
+         * This function isn't thread safe. Be sure to call it once, and before performing other operations.
+         *
+         * Check libsodium's documentation for more info.
+         */
+        public int sodium_init();
 
-		public String sodium_version_string();
+        public String sodium_version_string();
 
-		public static final int SHA256BYTES = 32;
+        public static final int SHA256BYTES = 32;
 
-		public int crypto_hash_sha256(@Out byte[] buffer, @In byte[] message,
-				@u_int64_t long sizeof);
+        public int crypto_hash_sha256(@Out byte[] buffer, @In byte[] message, @u_int64_t long sizeof);
 
-		public static final int SHA512BYTES = 64;
+        public static final int SHA512BYTES = 64;
 
-		public int crypto_hash_sha512(@Out byte[] buffer, @In byte[] message,
-				@u_int64_t long sizeof);
+        public int crypto_hash_sha512(@Out byte[] buffer, @In byte[] message, @u_int64_t long sizeof);
 
-		public static final int BLAKE2B_OUTBYTES = 64;
 
-		public int crypto_generichash_blake2b(@Out byte[] buffer,
-				@u_int64_t long outLen, @In byte[] message,
-				@u_int64_t long messageLen, @In byte[] key,
-				@u_int64_t long keyLen);
+        public static final int BLAKE2B_OUTBYTES = 64;
+        public int crypto_generichash_blake2b(@Out byte[] buffer, @u_int64_t long outLen,
+                                              @In byte[] message, @u_int64_t long messageLen,
+                                              @In byte[] key, @u_int64_t long keyLen);
+        public int crypto_generichash_blake2b_salt_personal(@Out byte[] buffer, @u_int64_t long outLen,
+                                                            @In byte[] message, @u_int64_t long messageLen,
+                                                            @In byte[] key,  @u_int64_t long keyLen,
+                                                            @In byte[] salt,
+                                                            @In byte[] personal);
 
-		public int crypto_generichash_blake2b_salt_personal(@Out byte[] buffer,
-				@u_int64_t long outLen, @In byte[] message,
-				@u_int64_t long messageLen, @In byte[] key,
-				@u_int64_t long keyLen, @In byte[] salt, @In byte[] personal);
+        public static final int PUBLICKEY_BYTES = 32;
+        public static final int SECRETKEY_BYTES = 32;
 
-		public static final int PUBLICKEY_BYTES = 32;
-		public static final int SECRETKEY_BYTES = 32;
+        public int crypto_box_curve25519xsalsa20poly1305_keypair(@Out byte[] publicKey, @Out byte[] secretKey);
 
-		public int crypto_box_curve25519xsalsa20poly1305_keypair(
-				@Out byte[] publicKey, @Out byte[] secretKey);
 
-		public static final int NONCE_BYTES = 24;
-		public static final int ZERO_BYTES = 32;
-		public static final int BOXZERO_BYTES = 16;
+        public static final int NONCE_BYTES = 24;
+        public static final int ZERO_BYTES = 32;
+        public static final int BOXZERO_BYTES = 16;
 
-		public void randombytes(@Out byte[] buffer, @u_int64_t long size);
+        public void randombytes(@Out byte[] buffer, @u_int64_t long size);
 
-		public int crypto_box_curve25519xsalsa20poly1305(@Out byte[] ct,
-				@In byte[] msg, @u_int64_t long length, @In byte[] nonce,
-				@In byte[] publicKey, @In byte[] privateKey);
+        public int crypto_box_curve25519xsalsa20poly1305(@Out byte[] ct, @In byte[] msg, @u_int64_t long length, @In byte[] nonce,
+                                                         @In byte[] publicKey, @In byte[] privateKey);
 
-		public int crypto_box_curve25519xsalsa20poly1305_open(
-				@Out byte[] message, @In byte[] ct, @u_int64_t long length,
-				@In byte[] nonce, @In byte[] publicKey, @In byte[] privateKey);
+        public int crypto_box_curve25519xsalsa20poly1305_open(@Out byte[] message, @In byte[] ct, @u_int64_t long length,
+                                                              @In byte[] nonce, @In byte[] publicKey, @In byte[] privateKey);
 
-		public static final int SCALAR_BYTES = 32;
+        public static final int SCALAR_BYTES = 32;
 
-		public int crypto_scalarmult_curve25519(@Out byte[] result,
-				@In byte[] intValue, @In byte[] point);
+        public int crypto_scalarmult_curve25519(@Out byte[] result, @In byte[] intValue, @In byte[] point);
 
-		public static final int XSALSA20_POLY1305_SECRETBOX_KEYBYTES = 32;
-		public static final int XSALSA20_POLY1305_SECRETBOX_NONCEBYTES = 24;
+        public static final int XSALSA20_POLY1305_SECRETBOX_KEYBYTES = 32;
+        public static final int XSALSA20_POLY1305_SECRETBOX_NONCEBYTES = 24;
 
-		int crypto_secretbox_xsalsa20poly1305(@Out byte[] ct, @In byte[] msg,
-				@u_int64_t long length, @In byte[] nonce, @In byte[] key);
+        int crypto_secretbox_xsalsa20poly1305(@Out byte[] ct, @In byte[] msg, @u_int64_t long length, @In byte[] nonce, @In byte[] key);
 
-		int crypto_secretbox_xsalsa20poly1305_open(@Out byte[] message,
-				@In byte[] ct, @u_int64_t long length, @In byte[] nonce,
-				@In byte[] key);
+        int crypto_secretbox_xsalsa20poly1305_open(@Out byte[] message, @In byte[] ct, @u_int64_t long length, @In byte[] nonce, @In byte[] key);
 
-		public static final int SIGNATURE_BYTES = 64;
+        public static final int SIGNATURE_BYTES = 64;
 
-		int crypto_sign_ed25519_seed_keypair(@Out byte[] publicKey,
-				@Out byte[] secretKey, @In byte[] seed);
+        int crypto_sign_ed25519_seed_keypair(@Out byte[] publicKey, @Out byte[] secretKey, @In byte[] seed);
 
-		int crypto_sign_ed25519(@Out byte[] buffer,
-				@Out LongLongByReference bufferLen, @In byte[] message,
-				@u_int64_t long length, @In byte[] secretKey);
+        int crypto_sign_ed25519(@Out byte[] buffer, @Out LongLongByReference bufferLen, @In byte[] message, @u_int64_t long length, @In byte[] secretKey);
 
-		int crypto_sign_ed25519_open(@Out byte[] buffer,
-				@Out LongLongByReference bufferLen, @In byte[] sigAndMsg,
-				@u_int64_t long length, @In byte[] key);
+        int crypto_sign_ed25519_open(@Out byte[] buffer, @Out LongLongByReference bufferLen, @In byte[] sigAndMsg, @u_int64_t long length, @In byte[] key);
+        
+        public static final int AES_128_CTR_KEYBYTES = 16;
+	public static final int CRYPTO_STREAM_AES_128_CTR_NONCEBYTES = 16;
 
-		public static final int AES_128_CTR_KEYBYTES = 16;
-		public static final int CRYPTO_STREAM_AES_128_CTR_NONCEBYTES = 16;
+	int crypto_stream_aes128ctr(@Out byte[] result, @In byte[] msg,	@u_int64_t long length, @In byte[] nonce, @In byte[] key);
 
-		int crypto_stream_aes128ctr(@Out byte[] result, @In byte[] msg,
-				@u_int64_t long length, @In byte[] nonce, @In byte[] key);
+	int crypto_stream_aes128ctr_xor(@Out byte[] result, @In byte[] msg,	@u_int64_t long length, @In byte[] nonce, @In byte[] key);
+    }
 
-		int crypto_stream_aes128ctr_xor(@Out byte[] result, @In byte[] msg,
-				@u_int64_t long length, @In byte[] nonce, @In byte[] key);
-
-	}
-
-	/**
-	 * This function isn't thread safe. Be sure to call it once, and before
-	 * performing other operations.
-	 * 
-	 * Check libsodium's <i>sodium_init()</i> documentation for more info.
-	 */
-	public static int init() {
-		return sodium().sodium_init();
-	}
+    /**
+     * This function isn't thread safe. Be sure to call it once, and before performing other operations.
+     *
+     * Check libsodium's <i>sodium_init()</i> documentation for more info.
+     */
+    public static int init() {
+        return sodium().sodium_init();
+    }
 }
